3c38327f3410b00c8a532a2c0e5c0f45bbde2e11
haoliasu <haoliasu@gmail.com>
2015-01-30 14:56:30 -0700

    multiThread1.0

---
+++ multiThread/multiThread/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiThread
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class manipulateNumInArray
+    {
+        public static void manipulate(int oper, int byNum, int[] array)
+        {
+            switch (oper)
+            { 
+                case 0:
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        array[i] = addTo(array[i], byNum);
+                    }
+                   break;
+
+                case 1:
+                   for (int i = 0; i < array.Length; i++)
+                   {
+                       array[i] = multi(array[i], byNum);
+                   }
+                    break;
+                default:
+                    break;
+
+            }
+        }
+        public static int addTo(int input,int adder)
+        {
+            return input + adder;
+        }
+
+        public static int multi(int input, int adder)
+        {
+            return input * adder;
+        }
+
+
+
+
+
+
+    }
+}
