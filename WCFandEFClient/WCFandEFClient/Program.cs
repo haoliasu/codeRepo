e0f1de5739dfaecf88c947931af63bfb1d0373b3
haoliasu <haoliasu@gmail.com>
2015-02-06 15:34:49 -0700

    EFWCFClient

---
+++ WCFandEFClient/WCFandEFClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFandEFClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client serviceClient = new ServiceReference1.Service1Client();
+            string result=serviceClient.GetData(1389);
+            Console.WriteLine(result);
+        }
+    }
+}
