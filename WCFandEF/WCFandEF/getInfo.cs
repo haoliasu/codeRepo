c7a3dea2895dda228ed4fcbfcbb82fe1ceeee534
haoliasu <haoliasu@gmail.com>
2015-02-06 15:34:04 -0700

    EFWCFSERVER

---
+++ WCFandEF/WCFandEF/getInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFandEF
+{
+    public class getInfo
+    {
+        public static string getPublisherName(int n)
+        {
+            using (pubsEntities context = new pubsEntities())
+            {
+                var result = (from pubName in context.publishers
+                                where pubName.pub_id.Equals( n.ToString())
+                                select pubName).First();
+                return result.pub_name;
+           }
+        }
+       
+    
+
+    
+    }
+}
